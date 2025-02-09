f3ef04849d22200cf1c4ad394b29446e4ecaee91
quandst <20127601@student.hcmus.edu.vn>
2025-02-09 09:26:05 +0700

    Implement basic API Gateway with CORS

---
+++ lib/apiGateway.ts
@@ -0,0 +1,45 @@
+import { CorsHttpMethod, HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { origins } from './utils';
+
+export class ApiGateway extends Construct {
+  public readonly httpApi: HttpApi;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.httpApi = this.createHttpApi();
+  }
+
+  private createHttpApi(): HttpApi {
+    const httpApi = new HttpApi(this, 'HttpApi', {
+      apiName: 'api',
+      corsPreflight: this.getCorsConfiguration(),
+    });
+
+    httpApi.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    return httpApi;
+  }
+
+  private getCorsConfiguration() {
+    return {
+      allowHeaders: [
+        'Content-Type',
+        'X-Amz-Date',
+        'Authorization',
+        'X-Api-Key',
+      ],
+      allowMethods: [
+        CorsHttpMethod.GET,
+        CorsHttpMethod.POST,
+        CorsHttpMethod.PUT,
+        CorsHttpMethod.DELETE,
+        // CorsHttpMethod.OPTIONS,
+        // CorsHttpMethod.PATCH,
+      ],
+      allowCredentials: true,
+      allowOrigins: origins,
+    };
+  }
+}
