3fe3532ba52b83dec74a129b9fc6020e1d3cacb0
quandst <20127601@student.hcmus.edu.vn>
2025-02-09 09:36:30 +0700

    Implement IAM roles with managed policies

---
+++ lib/identityManagement.ts
@@ -0,0 +1,54 @@
+import { Construct } from 'constructs';
+import { Role, ServicePrincipal, ManagedPolicy, CfnElement, RemovalPolicy } from 'aws-cdk-lib/aws-iam';
+
+export class IdentityManagement extends Construct {
+  public readonly AWSLambdaBasicExecutionRole: (id?: string) => Role;
+  public readonly AmazonCognitoPowerUser: (id?: string) => Role;
+  public readonly AmazonS3FullAccess: (id?: string) => Role;
+  public readonly AmazonDynamoDBFullAccess: (id?: string) => Role;
+  public readonly AmazonDynamoDBFullAccessWithSSMFullAccess: (id?: string) => Role;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.AmazonCognitoPowerUser = this.createRole('AmazonCognitoPowerUser', [
+      'service-role/AWSLambdaBasicExecutionRole',
+      'AmazonCognitoPowerUser'
+    ]);
+
+    this.AWSLambdaBasicExecutionRole = this.createRole('AWSLambdaBasicExecutionRole', [
+      'service-role/AWSLambdaBasicExecutionRole'
+    ]);
+
+    this.AmazonDynamoDBFullAccess = this.createRole('AmazonDynamoDBFullAccess', [
+      'service-role/AWSLambdaBasicExecutionRole',
+      'AmazonDynamoDBFullAccess'
+    ]);
+
+    this.AmazonDynamoDBFullAccessWithSSMFullAccess = this.createRole('AmazonDynamoDBFullAccessWithSSMFullAccess', [
+      'service-role/AWSLambdaBasicExecutionRole',
+      'AmazonDynamoDBFullAccess',
+      'AmazonSSMFullAccess'
+    ]);
+
+    this.AmazonS3FullAccess = this.createRole('AmazonS3FullAccess', [
+      'service-role/AWSLambdaBasicExecutionRole',
+      'AmazonDynamoDBFullAccess',
+      'AmazonSSMFullAccess',
+      'AmazonS3FullAccess'
+    ]);
+  }
+
+  private createRole(roleName: string, managedPolicies: string[]): (id?: string) => Role {
+    return (id = roleName) => {
+      const role = new Role(this, id, {
+        assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+        managedPolicies: managedPolicies.map(policy => ManagedPolicy.fromAwsManagedPolicyName(policy)),
+      });
+
+      (role.node.defaultChild as CfnElement).overrideLogicalId(id);
+      role.applyRemovalPolicy(RemovalPolicy.DESTROY);
+      return role;
+    };
+  }
+}
